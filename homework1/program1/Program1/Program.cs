b3c196aa87a610ad97276265ea678f661ece4b41
卢文宇 <43107924+Edward819@users.noreply.github.com>
2018-09-12 18:37:54 +0800

    Add files via upload

---
+++ homework1/program1/Program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class program1
+{
+    public static void Main(string[] args)
+    {
+        string n = "";
+        int a = 0, b = 0;
+        Console.Write("Please input the first number: ");
+        n = Console.ReadLine();
+        a = Int32.Parse(n);
+        Console.Write("Please input the second number: ");
+        n = Console.ReadLine();
+        b = Int32.Parse(n);
+        Console.WriteLine("You have entered:" + a + " and " + b);
+        int s = a * b;
+        Console.WriteLine("and their product is: " + s);
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+public class WinInOut:Form
+{
+    TextBox txt1 = new TextBox();
+    TextBox txt2 = new TextBox();
+    Button btn = new Button();
+    Label lbl = new Label();
+
+    public void init()
+    {
+        this.Controls.Add(txt1);
+        this.Controls.Add(txt2);
+        this.Controls.Add(btn);
+        this.Controls.Add(lbl);
+        txt1.Dock = System.Windows.Forms.DockStyle.Top;
+        txt2.Dock = System.Windows.Forms.DockStyle.Top;
+        btn.Dock = System.Windows.Forms.DockStyle.Fill;
+        lbl.Dock = System.Windows.Forms.DockStyle.Bottom;
+        btn.Text = "求乘积";
+        lbl.Text = "用于显示结果的标签";
+        this.Size = new Size(300, 120);
+
+        btn.Click += new System.EventHandler(this.button1_Click);
+    }
+
+    public void button1_Click(object sender, EventArgs e)
+    {
+        string s1 = txt1.Text, s2 = txt2.Text;
+        double a = double.Parse(s1), b = double.Parse(s2);
+        double n = a * b;
+        lbl.Text = a + "与" + b + "的乘积是：" + n;
+    }
+    static void Do()
+    {
+        WinInOut f = new WinInOut();
+        f.Text = "WinInOut";
+        f.init();
+        Application.Run(f);
+    }
+}
