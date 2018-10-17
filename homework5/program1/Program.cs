dab8782cc3f8d5792d83c403a5337f61a17b1825
卢文宇 <43107924+Edward819@users.noreply.github.com>
2018-10-17 20:40:43 +0800

    Add files via upload

---
+++ homework5/program1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OrderService.AddOrder(201701, "牙刷", "小李", 100);  
+            //OrderService.InquiryOrder(201701);           
+            //OrderService.AddOrder(201702, "手机", "小王", 2000); 
+            //OrderService.InquiryOrder(201702);           
+            //OrderService.AddOrder(201801, "灯泡", "小吴", 200); 
+            //OrderService.InquiryOrder(201801);           
+
+            //OrderService.RemoveOrder(201702);             
+            //OrderService.AmendOrder(201701, "牙刷2", "小李", 300);
+            //OrderService.InquiryOrder(201701);
+
+            //OrderService.RemoveOrder(124124124);          //删除订单失败
+            //OrderService.AmendOrder(2343325, "asdas", "asdasd", 200);//修改订单失败
+
+        }
+    }
+}
+       
+                 
