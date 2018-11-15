e38713c3a5be78ace4a26e3cb7c61ce7a9818995
卢文宇 <43107924+Edward819@users.noreply.github.com>
2018-11-15 19:26:34 +0800

    Add files via upload

---
+++ homework8/program1/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Order
+    {
+        List<OrderDetail> orders;
+        public Order()
+        {
+            orders = new List<OrderDetail>();
+        }
+        public void addNewOrder(OrderDetail anOrder)
+        {
+            orders.Add(anOrder);
+        }
+    }
+}
